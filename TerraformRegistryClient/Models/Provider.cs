265e3e79b5b4c604a80829a7d9bdae7df92498aa
Jeff Patton <jeffrey@patton-tech.com>
2024-08-02 17:04:45 -0500

    Adding XML Comments

--- TerraformRegistryClient/Models/Provider.cs
+++ TerraformRegistryClient/Models/Provider.cs
@@ -1,10 +1,28 @@
 ﻿namespace ModPosh.Models
 {
+    /// <summary>
+    /// Represents a provider used in a Terraform module.
+    /// </summary>
     public class Provider
     {
+        /// <summary>
+        /// Gets or sets the name of the provider.
+        /// </summary>
         public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the namespace of the provider.
+        /// </summary>
         public string NameSpace { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the source URL or location of the provider.
+        /// </summary>
         public string Source { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the version of the provider.
+        /// </summary>
         public string Version { get; set; } = string.Empty;
     }
 }
