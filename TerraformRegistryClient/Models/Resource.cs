b830b7fe9f685f47d19b547a596fb7b4fe7964ae
Jeff Patton <jeffrey@patton-tech.com>
2024-08-02 17:05:13 -0500

    Adding XML Comments

--- TerraformRegistryClient/Models/Resource.cs
+++ TerraformRegistryClient/Models/Resource.cs
@@ -1,8 +1,18 @@
 ﻿namespace ModPosh.Models
 {
+    /// <summary>
+    /// Represents a resource used in a Terraform module.
+    /// </summary>
     public class Resource
     {
+        /// <summary>
+        /// Gets or sets the name of the resource.
+        /// </summary>
         public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the type of the resource.
+        /// </summary>
         public string Type { get; set; } = string.Empty;
     }
 }
