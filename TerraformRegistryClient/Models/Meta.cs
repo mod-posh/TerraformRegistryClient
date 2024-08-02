941464361b4ca03a1cbf6257bbc83621cbd8e656
Jeff Patton <jeffrey@patton-tech.com>
2024-08-02 17:03:54 -0500

    Adding XML Comments

--- TerraformRegistryClient/Models/Meta.cs
+++ TerraformRegistryClient/Models/Meta.cs
@@ -1,10 +1,28 @@
 ﻿namespace ModPosh.Models
 {
+    /// <summary>
+    /// Represents metadata for pagination and navigation in API responses.
+    /// </summary>
     public class Meta
     {
+        /// <summary>
+        /// Gets or sets the limit of items per page.
+        /// </summary>
         public int Limit { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current offset of the items being fetched.
+        /// </summary>
         public int CurrentOffset { get; set; }
+
+        /// <summary>
+        /// Gets or sets the offset for the next set of items to be fetched.
+        /// </summary>
         public int NextOffset { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL for fetching the next set of items.
+        /// </summary>
         public string NextUrl { get; set; } = string.Empty;
     }
 }
